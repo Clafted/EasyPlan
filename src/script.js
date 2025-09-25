1da622940a45441060f0649f6f330b0b96cdcf26
Noah Miguel Perez <noahm.p.in2017@gmail.com>
2025-09-25 16:59:13 -0400

    Rescripted backend; New and nicer design!

--- src/script.js
+++ src/script.js
@@ -1,109 +1,117 @@
-var planGradient = "linear-gradient(to top, rgb(62, 9, 62) 60%, rgb(241, 220, 249))";
-var targetGradient = "linear-gradient(to top, rgba(100, 71, 119, 1) 20%, rgb(241, 220, 249))";
+var id_inc = 0;
+var plans = [];
+var planGradient = "linear-gradient(to top, rgb(40, 4, 40) 5%, rgb(62, 9, 62) 50%, rgba(0, 0, 0, 0))";
+var planShadow = "0mm 2mm 0mm rgb(20, 1, 20)";
+var targetGradient = "linear-gradient(to top, rgba(129, 15, 129, 1) 5%, rgba(94, 11, 94, 1) 50%, rgba(0, 0, 0, 0))";
+var targetShadow = "0mm 2mm 0mm rgba(65, 1, 65, 1)";
 var text = "";
+var targetPlan = null;
 var target = 0;
 var prop = 0;
-var plans = [];
 var prevKey;
 var doublePress = false;
 
 class Plan {
     id = 0;
-    data = ['', '', ''];
-    element = 0;
+    data = [];
+    element = null;
+    name = null;
+    date = null;
+    desc = null;
 
-    constructor(id) {
-        this.id = id;
+    constructor() {
+        this.id = id_inc++;
+        this.data = ["", "", ""];
         this.element = document.createElement("div");
-        let name = document.createElement("h1");
-        let date = document.createElement("h2");
-        let desc = document.createElement("p");
-        let idx = document.createElement('h3');
-        name.className = 'name';
-        date.className = 'date';
-        desc.className = 'desc';
-        idx.className = 'idx';
-        this.element.appendChild(name);
-        this.element.appendChild(date);
-        this.element.appendChild(desc);
-        this.element.appendChild(idx);
+        this.element.setAttribute("class", "plan");
+        this.element.setAttribute("id", this.id);
+        let nameEl = document.createElement("input");
+        let dateEl = document.createElement("input");
+        let descEl = document.createElement("input");
+        nameEl.setAttribute("class", "name");
+        dateEl.setAttribute("class", "date");
+        descEl.setAttribute("class", "desc");
+        nameEl.setAttribute("type", "text");
+        dateEl.setAttribute("type", "text");
+        descEl.setAttribute("type", "text");
+        this.element.appendChild(nameEl);
+        this.element.appendChild(dateEl);
+        this.element.appendChild(descEl);
+        this.name = nameEl;
+        this.date = dateEl;
+        this.desc = descEl;
+    }
+
+    exitPlan() {
+        this.element.style.backgroundImage = planGradient;
+        this.element.style.boxShadow = planShadow;
+        for (let input of this.element.children) {
+            input.setAttribute("readonly", "");
+        }
+    }
+
+    enterPlan() {
+        this.element.style.backgroundImage = targetGradient;
+        this.element.style.boxShadow = targetShadow;
+        for (let input of this.element.children) {
+            input.removeAttribute("readonly");
+        }
     }
 }
 
-
-function addPlan(plan) {
+function addPlan() {
+    let plan = new Plan();
     plans.push(plan);
     document.querySelector("#plans").appendChild(plan.element);
-    enterProp(0);
-}
-
-function exitPlan(planDiv) {
-    planDiv.style.backgroundImage = planGradient;
-    prop = 0;
-        
-}
-
-function enterPlan(planDiv) {
-    planDiv.style.backgroundImage = targetGradient;
-}
-
-function enterProp(idx) {
-    let plan = document.getElementById("plans").children[target];
-    plan.children[prop].style.backgroundColor = "rgb(50, 6, 50)";
-    prop = idx;
-    text = plan.children[prop].innerHTML;
-    plan.children[prop].style.backgroundColor = "rgb(21, 2, 21)";
 }
 
 function changeTarget(idx) {
-    prop = 0;
-    planElements = document.querySelector("#plans").children;
-    exitPlan(planElements[target]);
-    target = Math.min(plans.length-1, Math.max(0, idx));
-    text = plans[target].data[prop];
-    window.scrollTo(0, planElements[target].getBoundingClientRect().y);
-    enterPlan(planElements[target]);
+    console.log(idx);
+    if (idx < 0 || plans.length <= idx) return;
+    plans[target].exitPlan();
+    plans[idx].enterPlan();
+    target = idx;
+    targetPlan = plans[target];
 }
 
 function moveTarget(idx) {
-    if (idx < 0 || plans.length-1 < idx) return;
-    plans[idx].element.insertAdjacentElement((target < idx) ? 'afterend' : 'beforebegin', 
-                                                plans[target].element);
+    if (idx < 0 || plans.length <= idx || plans.length == 1) return;
     let temp = plans[target];
+    let swapElement = plans[idx];
     plans[target] = plans[idx];
     plans[idx] = temp;
+    target = idx;
     changeTarget(idx);
 }
 
+function relocatePlans() {
+    let plansDiv = document.querySelector("#plans");
+    let planElements = [];
+    while (plansDiv.children.length > 0) {
+        plansDiv.removeChild(plansDiv.firstChild);
+    }
+    for (let plan of plans) plansDiv.appendChild(plan.element);
+}
 
 function keydown(e) {
     doublePress = (e.key == prevKey && !doublePress);
-    
-    if (e.key == "ArrowUp") {
+    if (e.key == "ArrowUp") 
+    {
         if (e.ctrlKey) moveTarget(target-1);
         else changeTarget(target-1);
-    } else if (e.key == "ArrowDown") {
+        relocatePlans();
+        targetPlan.name.focus();
+    } else if (e.key == "ArrowDown") 
+    {
         if (e.ctrlKey) moveTarget(target+1);
         else changeTarget(target+1);
-    }
-    else if (e.key == "Tab") {
-        e.preventDefault();
-        enterProp((prop+1)%3);
-    }
-    else if (e.key == "Enter" && doublePress)     
+        relocatePlans();
+        targetPlan.name.focus();
+    } else if (e.key == "Enter" && doublePress)     
     {
-        addPlan(new Plan(plans.length));
+        addPlan();
         changeTarget(plans.length-1);
-    } else if (document.getElementById("plans").children.length > 0) 
-    {
-        if (e.key == "Backspace") text = text.slice(0, -1);
-        else if (e.key == "Enter") text += '\n';
-        else text += e.key;
-        plans[target].data[prop] = text;
-        document.getElementById("plans")
-                .children[target]
-                .children[prop]
-                .innerHTML = text;
+        targetPlan.name.focus();
     }
     prevKey = e.key;
 }
